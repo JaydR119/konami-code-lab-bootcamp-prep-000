1a49cee1a45765016b0b1bf081ec71fd36d695c7
Jayd Robinson <jaydr119@gmail.com>
2019-09-17 16:21:18 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,21 +14,20 @@ const codes = [
 let index = 0;
 
 document.body.addEventListener("keydown", (thingPressed) => {
-  const key = thingPressed.key 
-  console.log(key) 
-  
+  const key = thingPressed.key
+  console.log(key)
+
   if (codes[index] == key) {
-    ++index 
-  } 
-  
-  else {
-    idx = 0 
+    ++index
   }
-  
+
+  else {
+    idx = 0
+  }
+
   console.log(index)
   if (index == 10) {
     alert("Hurray!");
-    index = 0 
+    index = 0
   }
-}); 
-
+});
